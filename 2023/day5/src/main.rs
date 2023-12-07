a4317e5048d44e5a3ba18a8e6d177e74798b8f2c
Mattia Righetti <matt95.righetti@gmail.com>
2023-12-07 01:44:29 +0100

    aoc day 5

--- 2023/day5/src/main.rs
+++ 2023/day5/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
+    cmp,
     io::{self, Read, Write},
-    u64,
 };
 
 fn main() -> io::Result<()> {
@@ -22,22 +22,23 @@ fn part1(input: &str) -> io::Result<u64> {
         .collect();
 
     let mut mappings: Vec<Vec<Map>> = Vec::new();
-    for i in (1..parts.len()).rev() {
+    for i in 1..parts.len() {
         let maps: Vec<Map> = parts[i]
             .split('\n')
             .skip(1)
-            .flat_map(Map::try_from)
+            .filter(|x| !x.is_empty())
+            .map(Map::from)
             .collect();
         mappings.push(maps);
     }
 
     let mut locations: Vec<u64> = Vec::with_capacity(seeds.len());
     for seed in seeds {
-        let mut val = seed;
+        let mut value_map = seed;
         for i in 0..mappings.len() {
-            val = mappings[i].into_range_mapping(&val);
+            value_map = mappings[i].get_mapped_value(&value_map);
         }
-        locations.push(val);
+        locations.push(value_map);
     }
 
     Ok(locations.into_iter().min().unwrap())
@@ -46,21 +47,64 @@ fn part1(input: &str) -> io::Result<u64> {
 fn part2(input: &str) -> io::Result<u64> {
     let parts: Vec<&str> = input.split("\n\n").collect();
 
+    // tuple indicating start and end values of range
+    let mut seed_ranges: Vec<(u64, u64)> = parts[0]
+        .split(' ')
+        .skip(1)
+        .filter_map(|x| x.parse().ok())
+        .collect::<Vec<u64>>()
+        .chunks(2)
+        .map(|w| (w[0], w[0] + w[1]))
+        .collect();
+
     let mut mappings: Vec<Vec<Map>> = Vec::new();
     for i in 1..parts.len() {
-        let maps: Vec<Map> = parts[i]
+        let mapping: Vec<Map> = parts[i]
             .split('\n')
             .skip(1)
-            .flat_map(Map::try_from)
+            .filter(|x| !x.is_empty())
+            .map(Map::from)
             .collect();
 
-        mappings.push(maps);
+        mappings.push(mapping);
     }
 
-    Ok(0)
+    for range_map in mappings {
+        let mut seed_ranges_map: Vec<(u64, u64)> = Vec::new();
+        while let Some((start, end)) = seed_ranges.pop() {
+            let mut ta = Some((start, end));
+            for i in 0..range_map.len() {
+                if let Some((left_overlap, right_overlap)) = overlaps(start, end, &range_map[i]) {
+                    seed_ranges_map.push((
+                        left_overlap - range_map[i].src + range_map[i].dst,
+                        right_overlap - range_map[i].src + range_map[i].dst,
+                    ));
+
+                    if left_overlap > start {
+                        seed_ranges.push((start, left_overlap));
+                    }
+
+                    if right_overlap < end {
+                        seed_ranges.push((right_overlap, end));
+                    }
+
+                    ta = None;
+                }
+            }
+
+            if ta.is_some() {
+                seed_ranges_map.push(ta.unwrap());
+            }
+        }
+
+        seed_ranges = seed_ranges_map.clone();
+    }
+
+    seed_ranges.sort();
+    Ok(seed_ranges[0].0)
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 struct Map {
     src: u64,
     dst: u64,
@@ -74,28 +118,22 @@ impl Map {
     }
 }
 
-impl TryFrom<&str> for Map {
-    type Error = String;
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
+impl From<&str> for Map {
+    fn from(value: &str) -> Self {
         let vals: Vec<&str> = value.split(' ').collect();
-        if vals.len() != 3 {
-            return Err("not enough arguments".into());
-        }
-
         let dst = vals[0].parse().unwrap();
         let src = vals[1].parse().unwrap();
         let rng = vals[2].parse().unwrap();
-
-        Ok(Map { src, dst, rng })
+        Map { src, dst, rng }
     }
 }
 
 trait IntoRangeMapping {
-    fn into_range_mapping(&self, v: &u64) -> u64;
+    fn get_mapped_value(&self, v: &u64) -> u64;
 }
 
 impl IntoRangeMapping for Vec<Map> {
-    fn into_range_mapping(&self, v: &u64) -> u64 {
+    fn get_mapped_value(&self, v: &u64) -> u64 {
         for map in self {
             if (map.src..map.src + map.rng).contains(v) {
                 return map.dst + (v - map.src);
@@ -103,6 +141,16 @@ impl IntoRangeMapping for Vec<Map> {
         }
 
         *v
+    }
+}
+
+fn overlaps(r_start: u64, r_end: u64, m: &Map) -> Option<(u64, u64)> {
+    let left_overlap = cmp::max(r_start, m.src);
+    let right_overlap = cmp::min(r_end, m.src + m.rng);
+
+    match left_overlap < right_overlap {
+        true => Some((left_overlap, right_overlap)),
+        false => None,
     }
 }
 
@@ -145,18 +193,23 @@ humidity-to-location map:
 56 93 4";
 
     #[test]
+    fn test_map_parse() {
+        assert_eq!(Map::new(20, 30, 40), Map::from("30 20 40"));
+    }
+
+    #[test]
     fn test_range_mapping() {
         let rng_map = vec![Map::new(0, 5, 2), Map::new(6, 9, 3)];
-        assert_eq!(5, rng_map.into_range_mapping(&0));
-        assert_eq!(6, rng_map.into_range_mapping(&1));
-        assert_eq!(3, rng_map.into_range_mapping(&3));
-        assert_eq!(4, rng_map.into_range_mapping(&4));
-        assert_eq!(5, rng_map.into_range_mapping(&5));
-        assert_eq!(9, rng_map.into_range_mapping(&6));
-        assert_eq!(10, rng_map.into_range_mapping(&7));
-        assert_eq!(11, rng_map.into_range_mapping(&8));
-        assert_eq!(9, rng_map.into_range_mapping(&9));
-        assert_eq!(10, rng_map.into_range_mapping(&10));
+        assert_eq!(5, rng_map.get_mapped_value(&0));
+        assert_eq!(6, rng_map.get_mapped_value(&1));
+        assert_eq!(3, rng_map.get_mapped_value(&3));
+        assert_eq!(4, rng_map.get_mapped_value(&4));
+        assert_eq!(5, rng_map.get_mapped_value(&5));
+        assert_eq!(9, rng_map.get_mapped_value(&6));
+        assert_eq!(10, rng_map.get_mapped_value(&7));
+        assert_eq!(11, rng_map.get_mapped_value(&8));
+        assert_eq!(9, rng_map.get_mapped_value(&9));
+        assert_eq!(10, rng_map.get_mapped_value(&10));
     }
 
     #[test]
